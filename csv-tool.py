d55ebf13e88a926b37159f7d42a50325e0f6e542
Sandeep Sahoo <sandipsahoo2k2@gmail.com>
2019-05-06 16:35:43 -0400

    Update csv-tool.py

--- csv-tool.py
+++ csv-tool.py
@@ -147,3 +147,145 @@ def compare_views_data(esperFile, dbFile, ignore_mismatched_columns):
     diff_file.close()
     return
 
+def blankRow(columns):
+    newRow = []
+    for column in columns:
+        value = ''
+        newRow.append(value)
+
+    return newRow
+
+def blankFill(columns, filler):
+    newRow = []
+    for column in columns:
+        value = filler
+        newRow.append(value)
+
+    return newRow
+
+def readRow(columns, reader):
+    newRow = []
+    for column in columns:
+        try:
+            value = reader[column]
+        except KeyError:
+            value = ''
+        newRow.append(value)
+
+    return newRow
+
+def find_common_header(arr1, arr2):
+    rm = []
+    for d1 in arr1 :
+        if d1 in arr2 :
+            rm.append(d1)
+
+    return rm
+
+def get_mismatched_elements(arr1, arr2):
+
+    temp1 = arr1.copy()
+    temp2 = arr2.copy()
+    rm = []
+    for d1 in temp1 :
+        if d1 in temp2 :
+            rm.append(d1)
+
+    for d1 in rm:
+        temp1.remove(d1)
+        temp2.remove(d1)
+
+    return temp1 + temp2
+        
+def readDb(username, password, databaseName):
+    
+    try:
+        print("\nConnecting DB " + databaseName)
+        connection = cx_Oracle.connect (username,password,databaseName)
+        cursor = connection.cursor()
+        print("\nExecuting query..")
+        cursor.execute (DB_QUERY)
+        col_names = [row[0] for row in cursor.description]
+        result = cursor.fetchall()
+
+        dbFile = FILE_NAME + '_db.csv' ;
+        print("\nWritng to file " + dbFile)
+        with open(dbFile, 'w', newline='') as fp:
+            rswriter = csv.writer(fp, delimiter=',')
+            rswriter.writerow(col_names)
+            rswriter.writerows(result)
+            fp.close()
+        cursor.close()
+        connection.close()
+    except cx_Oracle.DatabaseError:
+        print ('Failed to connect to %s\n', databaseName)
+    return
+
+def readEsperView(url, sqlQuery, view, filter):
+    
+    encodedString = urllib.parse.quote_plus((sqlQuery + " " + view + " " + filter).encode('utf8'))
+    encodedString = encodedString.replace("%2A", "*", 1)
+    encodedUrl = url + encodedString;
+    print("\nEncodedUrl : " + encodedUrl + "\n")
+    with urlopen(encodedUrl) as response:
+        for line in response:
+            parsed_json = json.loads(line)
+        try:
+            return parsed_json['entries']
+        except KeyError:
+            print("No data fetched !!\n")
+            return None
+
+def writeToFile(entries):
+    
+    if entries is None:
+        print("\nNo data to write !!\n")
+        return
+    viewFile = FILE_NAME + '.csv' ;
+    print("\nWritng to file " + viewFile)
+    f = open(viewFile, 'w', newline='')
+    csvwriter = csv.writer(f)
+    headerwritten = 0
+    for entry in entries:
+        if headerwritten == 0 :
+            csvwriter.writerow(entry.keys())
+            headerwritten =+ 1
+        csvwriter.writerow(entry.values())
+    f.close()
+    return
+
+def sendMail(server, port, fromMailId, fromPwd, toMailId):
+
+    msg = MIMEMultipart()
+    msg['From'] = fromMailId
+    msg['To'] = toMailId
+    msg['Subject'] = "Here is the diff files attached"
+
+    body = "File for comparing " + FILE_NAME
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    attachment = open(FILE_NAME, "rb")
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % FILE_NAME)
+
+    msg.attach(part)
+
+    server = smtplib.SMTP(server, port)
+    server.starttls()
+    server.login(fromMailId, fromPwd)
+    text = msg.as_string()
+    server.sendmail(fromMailId, toMailId, text)
+    server.quit()
+
+if len(sys.argv) != 2:
+    print ("Usage : python " + sys.argv[0] + " <file.conf>")
+    sys.exit (1)
+
+readConfig(sys.argv[1])
+readDb(DB_USER, DB_PWD, DB_NAME)
+writeToFile(readEsperView(ESPER_BASE_URL, SELECT_STRING, ESPER_VIEW, FILTER_STRING))
+compare_views_data(ESPER_VIEW + '.csv', ESPER_VIEW + '_db.csv' , 0)
